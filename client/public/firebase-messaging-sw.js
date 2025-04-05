dae19505c7e3177355d6ffc7e4213efaae2bef81
Aftab Alam <aftabalamdlm@gmail.com>
2025-04-06 00:33:26 +0530

    removed sensitive info

--- client/public/firebase-messaging-sw.js
+++ client/public/firebase-messaging-sw.js
@@ -6,14 +6,10 @@ importScripts(
   "https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js"
 );
 
-export const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+const firebaseConfig = {
+  messagingSenderId: "629777589006",
 };
+
 firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
